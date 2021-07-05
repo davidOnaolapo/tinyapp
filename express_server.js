0ce1ddd55f618b57de68e718a003f530431549c2
David Onaolapo <talk2dawich@gmail.com>
2021-07-05 23:22:27 +0000

    Added a header to both routes

--- express_server.js
+++ express_server.js
@@ -14,6 +14,10 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -23,10 +27,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+app.get("/urls/:shortURL", (req, res) => {
+const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
