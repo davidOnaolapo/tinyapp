84ad00493a0bf8f0f3746ff1e4ef25cb63f78fc2
David Onaolapo <talk2dawich@gmail.com>
2021-07-06 00:23:56 +0000

    Implemented POST to grab user input from the form on url/new

--- express_server.js
+++ express_server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
@@ -37,6 +37,11 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
 const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
